77d763fa28a837b3e6b9ece2f578c253d3a79023
margaretmumbi16@gmail.com <margaretmumbi16@gmail.com>
2022-07-01 06:58:47 +0300

    Stack Implementation

---
+++ StackImplementation/CStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackImplementation
+{
+    class CStack
+    {
+
+        private int p_index;
+        private ArrayList list;
+
+        public CStack()
+        {
+            list = new ArrayList();
+            p_index = -1;
+        }
+
+        public void Push(object item)
+        {
+            list.Add(item);
+            p_index++;
+        }
+        public object Pop()
+        {
+            object obj = list[p_index];
+            list.RemoveAt(p_index);
+            p_index--;
+            return obj;
+
+        }
+        public object Peek()
+        {
+            //object obj = list[p_index];
+            //return obj;
+            return list[p_index];
+
+        }
+        public void Clear()
+        {
+            list.Clear();
+            p_index = -1;
+        }
+        public int Count
+        {
+            get
+            {
+
+                return list.Count;
+            }
+
+        }
+    }
+}
---
+++ StackImplementation/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace StackImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Icollection Stack Implementation
+            //Stack num = new Stack();
+            //Stack ops = new Stack();
+            //string expression = "5+10+15+20";
+            //calculate(num, ops, expression);
+            //Console.WriteLine(num.Pop());
+            //Console.Read();
+
+
+            //Stack class Implementation
+            CStack alist = new CStack();
+                string ch;
+                string word = "sees";
+                bool isPalindrome = true;
+                for (int x = 0; x < word.Length; x++)
+                    alist.Push(word.Substring(x, 1));
+                int pos = 0;
+                while (alist.Count > 0)
+                {
+                    ch = alist.Pop().ToString();
+                    if (ch != word.Substring(pos, 1))
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+                    pos++;
+                }
+                if (isPalindrome)
+                    Console.WriteLine(word + " is a palindrome.");
+                else
+                    Console.WriteLine(word + " is not a palindrome.");
+                Console.Read();
+            }
+
+            //IsNumeric is'nt built into c3 so we must define it
+            static bool IsNumeric(string input)
+            {
+                bool flag = true;
+                string pattern = (@"^\d+$");
+                Regex validate = new Regex(pattern);
+                if (!validate.IsMatch(input))
+                {
+                    flag = false;
+                }
+                return flag;
+            }
+
+            static void calculate(Stack N, Stack O, string exp)
+            {
+                string ch, token = "";
+                for (int p = 0; p < exp.Length; p++)
+                {
+                    ch = exp.Substring(p, 1);
+                    if (IsNumeric(ch))
+                        token += ch;
+                }
+
+
+            }
+        
+    }
+}
